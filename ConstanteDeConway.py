eb1a5f961264223132e9c415b7587f9422989696
popimardones <30806447+popimardones@users.noreply.github.com>
2017-11-13 13:50:14 -0300

    revision 2
    con lenguaje funcional

--- ConstanteDeConway.py
+++ ConstanteDeConway.py
@@ -1,23 +1,9 @@
-def lookAndSay(numero):
-    resultado = ""
-
-    repeat = numero[0]
-    numero = numero[1:] + " "
-    veces = 1
-
-    for actual in numero:
-        if actual != repeat:
-            resultado += str(veces) + repeat
-            veces = 1
-            repeat = actual
-        else:
-            veces += 1
-
-    return resultado
+import re
+x = "1"
+print(x)
+for n in range(10):
+    x="".join([str(len(j) + 1)+i for i, j in re.findall(r"(\d)(\1*)", x)])
+    print(x)
 
 
-num = "1"
 
-for i in range(10):
-    print(num)
-    num = lookAndSay(num)
